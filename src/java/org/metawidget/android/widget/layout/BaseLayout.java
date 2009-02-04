14e4737759b89f8aefb7f7581cbf4e594c9d30db
kennardconsult <kennardconsult@436cad57-ec4b-0410-b7f6-9007902e9484>
2009-02-04 22:16:34 +0000

    Upgrade Android

--- src/java/org/metawidget/android/widget/layout/BaseLayout.java
+++ src/java/org/metawidget/android/widget/layout/BaseLayout.java
@@ -35,10 +35,32 @@ public abstract class BaseLayout
 	implements Layout
 {
 	//
+	// Private statics
+	//
+
+	private final static int	VIEW_PADDING		= 0x0000000d;
+
+	private final static int	VIEW_PADDING_BOTTOM	= 0x00000011;
+
+	private final static int	VIEW_PADDING_LEFT	= 0x0000000e;
+
+	private final static int	VIEW_PADDING_RIGHT	= 0x00000010;
+
+	private final static int	VIEW_PADDING_TOP	= 0x0000000f;
+
+	private final static int[]	VIEW				= new int[] { VIEW_PADDING, VIEW_PADDING_BOTTOM, VIEW_PADDING_LEFT, VIEW_PADDING_RIGHT, VIEW_PADDING_TOP };
+
+	private final static int	TEXTVIEW_COLOR		= 0x00000005;
+
+	private final static int	TEXTVIEW_SIZE		= 0x00000002;
+
+	private final static int[]	TEXTVIEW			= new int[] { TEXTVIEW_COLOR, TEXTVIEW_SIZE };
+
+	//
 	// Private members
 	//
 
-	private AndroidMetawidget				mMetawidget;
+	private AndroidMetawidget	mMetawidget;
 
 	//
 	// Constructor
@@ -78,17 +100,20 @@ public abstract class BaseLayout
 	}
 
 	/**
-	 * Applies the given style id (defined in <code>res/values/styles.xml</code>) to the given View.
+	 * Applies the given style id (defined in <code>res/values/styles.xml</code>) to the given
+	 * View.
 	 */
 
-	// It isn't clear in 1.0_r1 how one is meant to generically and programmatically apply styles from their ids. In particular:
+	// It isn't clear in 1.0_r1 how one is meant to generically and programmatically apply styles
+	// from their ids. In particular:
 	//
 	// 1. AssetManager.applyStyle is package-protected; and
-	// 2. Theme.obtainStyledAttributes( AttributeSet, int, int, int ) has an explicit cast to XmlBlock$Parser (not just AttributeSet)
+	// 2. Theme.obtainStyledAttributes( AttributeSet, int, int, int ) has an explicit cast to
+	// XmlBlock$Parser (not just AttributeSet)
 	//
-	// So, this method is hard-coded for now. It is enough to convey the general idea though (eg. being able to set paramLabelStyle and
-	// paramSectionStyle externally).
-
+	// So, this method is hard-coded for now. It is enough to convey the general idea though (eg.
+	// being able to set paramLabelStyle and paramSectionStyle externally).
+	//
 	protected void applyStyle( View view, int style )
 	{
 		// No style?
@@ -98,11 +123,11 @@ public abstract class BaseLayout
 
 		// Construct StyledAttributes
 
-		TypedArray attributes = getMetawidget().getContext().obtainStyledAttributes( style, android.R.styleable.View );
+		TypedArray attributes = getMetawidget().getContext().obtainStyledAttributes( style, VIEW );
 
 		// Padding
 
-		int padding = attributes.getDimensionPixelSize( android.R.styleable.View_padding, -1 );
+		int padding = attributes.getDimensionPixelSize( VIEW_PADDING, -1 );
 		int leftPadding;
 		int topPadding;
 		int rightPadding;
@@ -117,29 +142,29 @@ public abstract class BaseLayout
 		}
 		else
 		{
-			leftPadding = attributes.getDimensionPixelSize( android.R.styleable.View_paddingLeft, 0 );
-			topPadding = attributes.getDimensionPixelSize( android.R.styleable.View_paddingTop, 0 );
-			rightPadding = attributes.getDimensionPixelSize( android.R.styleable.View_paddingRight, 0 );
-			bottomPadding = attributes.getDimensionPixelSize( android.R.styleable.View_paddingBottom, 0 );
+			leftPadding = attributes.getDimensionPixelSize( VIEW_PADDING_LEFT, 0 );
+			topPadding = attributes.getDimensionPixelSize( VIEW_PADDING_TOP, 0 );
+			rightPadding = attributes.getDimensionPixelSize( VIEW_PADDING_RIGHT, 0 );
+			bottomPadding = attributes.getDimensionPixelSize( VIEW_PADDING_BOTTOM, 0 );
 		}
 
 		view.setPadding( leftPadding, topPadding, rightPadding, bottomPadding );
 
 		if ( view instanceof TextView )
 		{
-			attributes = getMetawidget().getContext().obtainStyledAttributes( style, android.R.styleable.TextView );
+			attributes = getMetawidget().getContext().obtainStyledAttributes( style, TEXTVIEW );
 			TextView textView = (TextView) view;
 
 			// Color
 
-			ColorStateList colors = attributes.getColorStateList( android.R.styleable.TextView_textColor );
+			ColorStateList colors = attributes.getColorStateList( TEXTVIEW_COLOR );
 
 			if ( colors != null )
 				textView.setTextColor( colors );
 
 			// Size
 
-			textView.setTextSize( attributes.getDimensionPixelSize( android.R.styleable.TextView_textSize, 15 ) );
+			textView.setTextSize( attributes.getDimensionPixelSize( TEXTVIEW_SIZE, 15 ) );
 		}
 	}
 }
