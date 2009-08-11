f5a57d488ee3ffa5df1133bbc8fd46808578e315
kennardconsult <kennardconsult@436cad57-ec4b-0410-b7f6-9007902e9484>
2009-08-11 07:34:15 +0000

    Unit tests

--- src/java/org/metawidget/jsp/package-info.java
+++ src/java/org/metawidget/jsp/package-info.java
@@ -15,7 +15,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 /**
- * <strong>JSP support, including technologies based on JSP</strong>
+ * <strong>JSP support, including technologies based on JSP</strong>.
  *
  * @author Richard Kennard
  */
