28916b6805c6f6a8c386bc9492057b8a539b1601
kennardconsult <kennardconsult@436cad57-ec4b-0410-b7f6-9007902e9484>
2009-12-08 02:07:54 +0000

    Unit tests

--- src/java/org/metawidget/pipeline/base/package-info.java
+++ src/java/org/metawidget/pipeline/base/package-info.java
@@ -15,9 +15,9 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 /**
- * Metawidget convenience implementations (org.w3c-based mixin).
+ * Pipeline convenience implementations.
  *
  * @author Richard Kennard
  */
 
-package org.metawidget.mixin.w3c;
+package org.metawidget.pipeline.base;
--- src/java/org/metawidget/pipeline/w3c/package-info.java
+++ src/java/org/metawidget/pipeline/w3c/package-info.java
@@ -15,9 +15,9 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 /**
- * Metawidget convenience implementations (mixin).
+ * Metawidget convenience implementations (org.w3c-based pipeline).
  *
  * @author Richard Kennard
  */
 
-package org.metawidget.mixin.base;
+package org.metawidget.pipeline.w3c;
