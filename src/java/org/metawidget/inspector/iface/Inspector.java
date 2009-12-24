5d963bf70565886fcd558260092dd79981e7d54a
kennardconsult <kennardconsult@436cad57-ec4b-0410-b7f6-9007902e9484>
2009-12-24 05:13:23 +0000

    JavaDoc

--- src/java/org/metawidget/inspector/iface/Inspector.java
+++ src/java/org/metawidget/inspector/iface/Inspector.java
@@ -20,10 +20,29 @@ import org.metawidget.iface.Immutable;
 
 /**
  * Common interface implemented by all Inspectors. Inspectors decouple the process of generating
- * inspection results from back-end metadata.
+ * inspection results out of back-end metadata.
  * <p>
  * Inspectors must be immutable (or, at least, appear that way to clients. They can have caches or
  * configuration settings internally, as long as they are threadsafe).
+ * <p>
+ * <h2>Note to Implementors</h2>
+ * <p>
+ * This interface is designed to work for the <em>majority</em> of use cases, but not <em>every</em>
+ * use case. Inevitably there are tradeoffs. For example, the <code>toInspect</code> parameter is
+ * redundant for XML-based Inspectors (note, however, that the <code>type</code> parameter is
+ * <em>not</em> redundant for Object-based Inspectors).
+ * <p>
+ * In particular, this interface does not pass an <code>M metawidget</code> or any other kind of
+ * 'helper context' as the other interfaces do. This is because Inspectors are meant to exist independent
+ * of any particular UI framework. Indeed, they can exist on back-end tiers where no UI framework is
+ * available at all. This seems to work for most use cases encountered so far. Theoretically, though, if you
+ * find yourself needing access to a context, consider:
+ * <ul>
+ * <li>passing it in a Thread local (<code>JspAnnotationInspector</code> does this); or</li>
+ * <li>passing an <code>Object[]</code> containing the object to be inspected <em>and</em> the context to <code>toInspect</code>, and
+ * build something like <code>CompositeInspector</code> to strip that context back out before calling the regular <code>Inspectors</code>; or</li>
+ * <li>use an <code>InspectionResultProcessor</code>.</li>
+ * </ul>
  *
  * @author Richard Kennard
  */
