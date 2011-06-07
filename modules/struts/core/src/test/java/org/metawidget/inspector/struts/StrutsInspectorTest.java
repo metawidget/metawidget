a4a9a01d853dea3eeeae3f5241c2649f9aea283e
kennardconsult <kennardconsult@436cad57-ec4b-0410-b7f6-9007902e9484>
2011-06-07 06:57:12 +0000

    CDI support: more fine-grained dependencies (Weld crashes if metawidget-faces.jar is in the EJB tier, but we need the annotations in it)

---
+++ modules/struts/core/src/test/java/org/metawidget/inspector/struts/StrutsInspectorTest.java
@@ -0,0 +1,82 @@
+// Metawidget
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+package org.metawidget.inspector.struts;
+
+import static org.metawidget.inspector.InspectionResultConstants.*;
+import junit.framework.TestCase;
+
+import org.metawidget.config.ConfigReader;
+import org.metawidget.util.XmlUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * @author Richard Kennard
+ */
+
+public class StrutsInspectorTest
+	extends TestCase {
+
+	//
+	// Public methods
+	//
+
+	public void testInspection() {
+
+		StrutsInspectorConfig config = new StrutsInspectorConfig();
+		ConfigReader configReader = new ConfigReader();
+		config.setInputStreams( configReader.openResource( "org/metawidget/inspector/struts/test-struts-config1.xml" ), configReader.openResource( "org/metawidget/inspector/struts/test-struts-config2.xml" ) );
+		StrutsInspector inspector = new StrutsInspector( config );
+
+		Document document = XmlUtils.documentFromString( inspector.inspect( null, "testForm1" ) );
+
+		assertEquals( "inspection-result", document.getFirstChild().getNodeName() );
+
+		// Entity
+
+		Element entity = (Element) document.getFirstChild().getFirstChild();
+		assertEquals( ENTITY, entity.getNodeName() );
+		assertEquals( "testForm1", entity.getAttribute( TYPE ) );
+		assertFalse( entity.hasAttribute( NAME ) );
+
+		// Properties
+
+		Element property = (Element) entity.getFirstChild();
+		assertEquals( PROPERTY, property.getNodeName() );
+		assertEquals( "foo", property.getAttribute( NAME ) );
+		assertEquals( String.class.getName(), property.getAttribute( TYPE ) );
+		assertTrue( property.getAttributes().getLength() == 2 );
+
+		property = (Element) property.getNextSibling();
+		assertEquals( PROPERTY, property.getNodeName() );
+		assertEquals( "bar", property.getAttribute( NAME ) );
+		assertEquals( String.class.getName(), property.getAttribute( TYPE ) );
+		assertTrue( property.getAttributes().getLength() == 2 );
+
+		property = (Element) property.getNextSibling();
+		assertEquals( PROPERTY, property.getNodeName() );
+		assertEquals( "baz", property.getAttribute( NAME ) );
+		assertEquals( String.class.getName(), property.getAttribute( TYPE ) );
+		assertTrue( property.getAttributes().getLength() == 2 );
+
+		property = (Element) property.getNextSibling();
+		assertEquals( PROPERTY, property.getNodeName() );
+		assertEquals( "abc", property.getAttribute( NAME ) );
+		assertEquals( String.class.getName(), property.getAttribute( TYPE ) );
+		assertTrue( property.getAttributes().getLength() == 2 );
+	}
+}
