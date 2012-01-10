5ed031c4e8178a393445b4e9d570d89cfa14ca32
Ryan Bradley <rbradley@redhat.com>
2012-01-10 11:42:13 -0500

    Enhance StaticSpringAllWidgetsTest.
    Support for nested metawidgets, collections, and doAction not debugged yet.

--- integration-tests/static/spring/allwidgets/src/test/java/org/metawidget/statically/spring/StaticSpringAllWidgetsTest.java
+++ integration-tests/static/spring/allwidgets/src/test/java/org/metawidget/statically/spring/StaticSpringAllWidgetsTest.java
@@ -36,14 +36,79 @@ public class StaticSpringAllWidgetsTest
         metawidget.setValue( "${allWidgets}" );
         metawidget.setPath( AllWidgets.class.getName() );
         
-        String result = "<table>" +
-            "\t<form:input path=\"${allWidgets.mTextbox}\" value=\"Textbox\"/>\r\n" +
-            "\t<form:input path=\"${allWidgets.mLimitedTextbox}\" value=\"Limited Textbox\"/>\r\n" +
-            "\t<form:input path=\"${allWidgets.mTextarea\" value=\"Textarea\"/>/r/n" +
-            "\t<form:input path=\"${allWidgets.mPassword}\" value=\"Password\"/>/r/n" +
-            "\t<form:input path=\"${allWidgets.mBytePrimitive}\" value=\"1111111\"/>/r/n" +
-            "\t<form:input path=\"${allWidgets.mByteObject}\" value=\"0000000\"/>/r/n" +
-            "\t<form:input path=\"${allWidgets.mShortPrimitive}\" value\"\"/>/r/n"
+        String result = "<table>\r\n" +
+            "\t<form:input path=\"textbox\"/>\r\n" +
+            "\t<form:input maxLength=\"20\" path=\"limitedTextbox\"/>\r\n" +
+            "\t<textarea path=\"textarea\"/>\r\n" +
+            "\t<input path=\"password\" type=\"secret\"/>\r\n" +
+            "\t<form:input path=\"bytePrimitive\"/>\r\n" +
+            "\t<form:input path=\"byteObject\"/>\r\n" +
+            "\t<form:input path=\"shortPrimitive\"/>\r\n" +
+            "\t<form:input path=\"shortObject\"/>\r\n" +
+            "\t<form:input path=\"intPrimitive\"/>\r\n" +
+            "\t<form:input path=\"integerObject\"/>\r\n" +
+            "\t<form:input path=\"rangedInt\"/>\r\n" +
+            "\t<form:input path=\"rangedInteger\"/>\r\n" +
+            "\t<form:input path=\"longPrimitive\"/>\r\n" +
+            "\t<form:input path=\"longObject\"/>\r\n" +
+            "\t<form:input path=\"floatPrimitive\"/>\r\n" +
+            "\t<form:input path=\"floatObject\"/>\r\n" +
+            "\t<form:input path=\"doublePrimitive\"/>\r\n" +
+            "\t<form:input path=\"doubleObject\"/>\r\n" +
+            "\t<form:input maxLength=\"1\" path=\"charPrimitive\"/>\r\n" +
+            "\t<form:input maxLength=\"1\" path=\"characterObject\"/>\r\n" +
+            "\t<input path=\"booleanPrimitive\" type=\"checkbox\"/>\r\n" +
+            "\t<form:select path=\"booleanObject\">\r\n" +
+            "\t\t<form:option/>\r\n" +
+            "\t\t<form:option value=\"true\">Yes</form:option>\r\n" +
+            "\t\t<form:option value=\"false\">No</form:option>\r\n" +
+            "\t</form:select>\r\n" +
+            "\t<form:select path=\"dropdown\">\r\n" +
+            "\t\t<form:option/>\r\n" +
+            "\t\t<form:option value=\"foo1\"/>\r\n" +
+            "\t\t<form:option value=\"dropdown1\"/>\r\n" +
+            "\t\t<form:option value=\"bar1\"/>\r\n" +
+            "\t</form:select>\r\n" +
+            "\t<form:select path=\"dropdownWithLabels\">\r\n" +
+            "\t\t<form:option/>\r\n" +
+            "\t\t<form:option value=\"foo2\">Foo #2</form:option>\r\n" +
+            "\t\t<form:option value=\"dropdown2\">Dropdown #2</form:option>\r\n" +
+            "\t\t<form:option value=\"bar2\">Bar #2</form:option>\r\n" +
+            "\t\t<form:option value=\"baz2\">Baz #2</form:option>\r\n" +
+            "\t</form:select>\r\n" +
+            "\t<form:select path=\"notNullDropdown\">\r\n" +
+            "\t\t<form:option value=\"-1\"/>\r\n" +
+            "\t\t<form:option value=\"0\"/>\r\n" +
+            "\t\t<form:option value=\"1\"/>\r\n" +
+            "\t</form:select>\r\n" +
+            "\t<form:select path=\"notNullObjectDropdown\">\r\n" +
+            "\t\t<form:option value=\"foo3\"/>\r\n" +
+            "\t\t<form:option value=\"dropdown3\"/>\r\n" +
+            "\t\t<form:option value=\"bar3\"/>\r\n" +
+            "\t\t<form:option value=\"baz3\"/>\r\n" +
+            "\t\t<form:option value=\"abc3\"/>\r\n" +
+            "\t\t<form:option value=\"def3\"/>\r\n" +
+            "\t</form:select>\r\n" +
+            
+            /*
+             * Nested Metawidget support is not fully debugged yet. 
+             */
+            
+            "\t<table/>\r\n" +
+            "\t<table/>\r\n" +
+            "\t<form:input path=\"nestedWidgetsDontExpand\"/>\r\n" +
+            "\t<span path=\"readOnlyNestedWidgetsDontExpand\"/>\r\n" +
+            "\t<form:input path=\"date\"/>\r\n" +
+            "\t<form:hidden path=\"hidden\"/>\r\n" +
+            "\t<span path=\"readOnly\"/>\r\n" +
+            "\t<form:input path=\"mystery\"/>\r\n" +
+
+            /*
+             * Collection and doAction not handled properly yet.
+             */
+            
+            "</table>\r\n" +
+            "<form:hidden path=\"hidden\"/>\r\n"
             ;
         
         StringWriter writer = new StringWriter();
