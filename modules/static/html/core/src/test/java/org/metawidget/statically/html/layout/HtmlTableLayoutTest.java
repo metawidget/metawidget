19e66e693ca1af3c260cb7065511b1c5368bc3af
Richard Kennard <richard@kennardconsulting.com>
2012-07-08 19:43:26 +1000

    Unit test for https://github.com/metawidget/metawidget/pull/17
    Change-Id: Ic2a2f564725bee257bf037eaa528ce391fc35fba

---
+++ modules/static/html/core/src/test/java/org/metawidget/statically/html/layout/HtmlTableLayoutTest.java
@@ -0,0 +1,66 @@
+// Metawidget
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+package org.metawidget.statically.html.layout;
+
+import static org.metawidget.inspector.InspectionResultConstants.*;
+
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+import org.metawidget.statically.html.StaticHtmlMetawidget;
+import org.metawidget.statically.html.widgetbuilder.HtmlDiv;
+import org.metawidget.statically.html.widgetbuilder.HtmlInput;
+import org.metawidget.util.CollectionUtils;
+
+public class HtmlTableLayoutTest
+	extends TestCase {
+
+	//
+	// Public methods
+	//
+
+	public void testHtmlTableLayout()
+		throws Exception {
+
+		HtmlTableLayoutConfig config = new HtmlTableLayoutConfig();
+		config.setTableStyle( "tableStyle" );
+		config.setTableStyleClass( "tableStyleClass" );
+		config.setLabelColumnStyleClass( "labelColumnStyleClass" );
+		config.setComponentColumnStyleClass( "componentColumnStyleClass" );
+		config.setRequiredColumnStyleClass( "requiredColumnStyleClass" );
+		HtmlTableLayout layout = new HtmlTableLayout( config );
+
+		StaticHtmlMetawidget metawidget = new StaticHtmlMetawidget();
+		HtmlDiv container = new HtmlDiv();
+		Map<String, String> attributes = CollectionUtils.newHashMap();
+		attributes.put( NAME, "foo" );
+		HtmlInput htmlInput = new HtmlInput();
+		htmlInput.putAttribute( "id", "foo-bar" );
+		layout.startContainerLayout( container, metawidget );
+		layout.layoutWidget( htmlInput, PROPERTY, attributes, container, metawidget );
+		layout.endContainerLayout( container, metawidget );
+
+		StringBuilder builder = new StringBuilder();
+		builder.append( "<div><table class=\"tableStyleClass\" style=\"tableStyle\"><tbody><tr>" );
+		builder.append( "<th class=\"labelColumnStyleClass\"><label for=\"foo-bar\">Foo:</label></th>" );
+		builder.append( "<td class=\"componentColumnStyleClass\"><input id=\"foo-bar\"/></td>" );
+		builder.append( "<td class=\"requiredColumnStyleClass\"/>" );
+		builder.append( "</tr></tbody></table></div>" );
+		assertEquals( builder.toString(), container.toString() );
+	}
+}
