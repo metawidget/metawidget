64ee4129bc58f678bd04bb7a6c5820f35c806585
Richard Kennard <richard@kennardconsulting.com>
2012-09-12 14:57:17 +1000

    Whitepaper
    Change-Id: I7a012e71f3b09194c34ebc386a60c2da00f05bb1

--- website/core/src/main/webapp/documentation.php
+++ website/core/src/main/webapp/documentation.php
@@ -55,6 +55,7 @@
 		<ul>
 			<li><a href="media/whitepaper/MetawidgetWhitePaper-WhatGoodIsAnOIM.pdf">What Good is an OIM?</a></li>
 			<li><a href="media/whitepaper/MetawidgetWhitePaper-TelefonicaHealthPortal.pdf">Case Study: Telef&oacute;nica Health Portal</a></li>
+			<li><a href="media/whitepaper/MetawidgetWhitePaper-JBossForge.pdf">Case Study: JBoss Forge</a></li>
 			<li><a href="media/whitepaper/MetawidgetWhitePaper-AdoptionStudies.pdf">Adoption Studies</a></li>
 			<li><a href="media/whitepaper/MetawidgetWhitePaper-DuplicationInUserInterfaces.pdf">Duplication in User Interfaces</a></li>
 		</ul>
