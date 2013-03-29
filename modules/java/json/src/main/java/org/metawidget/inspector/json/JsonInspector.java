e20ff3e5396b3b40030ba630e1674112fba58404
Richard Kennard <richard@kennardconsulting.com>
2013-03-29 12:12:05 +1100

    JsonInspector
    Change-Id: Ifd767b8543fea99d64b6f8a9da318b6d64b4a1b2

---
+++ modules/java/json/src/main/java/org/metawidget/inspector/json/JsonInspector.java
@@ -0,0 +1,130 @@
+// Metawidget (licensed under LGPL)
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+package org.metawidget.inspector.json;
+
+import static org.metawidget.inspector.InspectionResultConstants.*;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+import org.metawidget.inspector.iface.DomInspector;
+import org.metawidget.inspector.iface.InspectorException;
+import org.metawidget.util.XmlUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+
+/**
+ * Inspector to look for metadata in JSON files.
+ *
+ * @author Richard Kennard
+ */
+
+public class JsonInspector
+	implements DomInspector<Element> {
+
+	//
+	// Private members
+	//
+
+	private JsonObject	mRoot;
+
+	//
+	// Constructor
+	//
+
+	public JsonInspector( JsonInspectorConfig config ) {
+
+		InputStream inputStream = config.getInputStream();
+
+		if ( inputStream == null ) {
+			throw InspectorException.newException( "No JSON input stream specified" );
+		}
+
+		// Parse the JSON
+
+		mRoot = (JsonObject) new JsonParser().parse( new InputStreamReader( inputStream ) );
+	}
+
+	//
+	// Protected methods
+	//
+
+	public final String inspect( Object toInspect, String type, String... names ) {
+
+		Element element = inspectAsDom( toInspect, type, names );
+
+		if ( element == null ) {
+			return null;
+		}
+
+		return XmlUtils.nodeToString( element, false );
+	}
+
+	public Element inspectAsDom( Object toInspect, String type, String... names ) {
+
+		// Start the DOM
+
+		Document document = XmlUtils.newDocument();
+		Element root = document.createElementNS( NAMESPACE, ROOT );
+		root.setAttribute( VERSION, "1.0" );
+		document.appendChild( root );
+		Element entity = document.createElementNS( NAMESPACE, ENTITY );
+		entity.setAttribute( TYPE, type );
+		root.appendChild( entity );
+
+		// Write all JSON values into it
+
+		for ( Map.Entry<String, JsonElement> entry : mRoot.entrySet() ) {
+
+			JsonElement element = entry.getValue();
+
+			// Write the name
+
+			Element child = document.createElementNS( NAMESPACE, PROPERTY );
+			child.setAttribute( NAME, entry.getKey() );
+
+			// Write the type
+
+			if ( element.isJsonPrimitive() ) {
+
+				JsonPrimitive primitive = (JsonPrimitive) element;
+
+				if ( primitive.isNumber() ) {
+					child.setAttribute( TYPE, int.class.getName() );
+				} else if ( primitive.isBoolean() ) {
+					child.setAttribute( TYPE, boolean.class.getName() );
+				} else {
+					child.setAttribute( TYPE, String.class.getName() );
+				}
+			} else if ( element.isJsonArray() ) {
+				child.setAttribute( TYPE, "array" );
+			}
+
+			entity.appendChild( child );
+		}
+
+		// Return the DOM
+
+		return root;
+	}
+}
---
+++ modules/java/json/src/test/java/org/metawidget/inspector/json/JsonInspectorTest.java
@@ -0,0 +1,99 @@
+// Metawidget (licensed under LGPL)
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
+package org.metawidget.inspector.json;
+
+import static org.metawidget.inspector.InspectionResultConstants.*;
+
+import java.io.ByteArrayInputStream;
+
+import junit.framework.TestCase;
+
+import org.metawidget.inspector.iface.InspectorException;
+import org.metawidget.util.MetawidgetTestUtils;
+import org.metawidget.util.XmlUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ * @author Richard Kennard
+ */
+
+public class JsonInspectorTest
+	extends TestCase {
+
+	@SuppressWarnings( "unused" )
+	public void testMissingFile() {
+
+		try {
+			new JsonInspector( new JsonInspectorConfig() );
+			fail();
+		} catch ( InspectorException e ) {
+			assertEquals( "No JSON input stream specified", e.getMessage() );
+		}
+	}
+
+	public void testNormalUse() {
+
+		String json = "{ \"foo\": \"Foo\", \"bar\": 42, \"baz\": true, \"abc\": [ 1, 2, 3 ] }";
+		JsonInspector inspector = new JsonInspector( new JsonInspectorConfig().setInputStream( new ByteArrayInputStream( json.getBytes() ) ) );
+		Document document = XmlUtils.documentFromString( inspector.inspect( null, "fooObject" ) );
+
+		assertEquals( "inspection-result", document.getFirstChild().getNodeName() );
+
+		// Entity
+
+		Element entity = (Element) document.getDocumentElement().getFirstChild();
+		assertEquals( ENTITY, entity.getNodeName() );
+		assertFalse( entity.hasAttribute( NAME ) );
+		assertEquals( "fooObject", entity.getAttribute( TYPE ) );
+
+		// Properties
+
+		Element property = (Element) entity.getFirstChild();
+		assertEquals( PROPERTY, property.getNodeName() );
+		assertEquals( "foo", property.getAttribute( NAME ) );
+		assertEquals( String.class.getName(), property.getAttribute( TYPE ) );
+		assertEquals( property.getAttributes().getLength(), 2 );
+
+		property = (Element) property.getNextSibling();
+		assertEquals( PROPERTY, property.getNodeName() );
+		assertEquals( "bar", property.getAttribute( NAME ) );
+		assertEquals( int.class.getName(), property.getAttribute( TYPE ) );
+		assertEquals( property.getAttributes().getLength(), 2 );
+
+		property = (Element) property.getNextSibling();
+		assertEquals( PROPERTY, property.getNodeName() );
+		assertEquals( "baz", property.getAttribute( NAME ) );
+		assertEquals( boolean.class.getName(), property.getAttribute( TYPE ) );
+		assertEquals( property.getAttributes().getLength(), 2 );
+
+		property = (Element) property.getNextSibling();
+		assertEquals( PROPERTY, property.getNodeName() );
+		assertEquals( "abc", property.getAttribute( NAME ) );
+		assertEquals( "array", property.getAttribute( TYPE ) );
+		assertEquals( property.getAttributes().getLength(), 2 );
+
+		assertEquals( property.getNextSibling(), null );
+	}
+
+	public void testConfig() {
+
+		MetawidgetTestUtils.testEqualsAndHashcode( JsonInspectorConfig.class, new JsonInspectorConfig() {
+			// Subclass
+		} );
+	}
+}
