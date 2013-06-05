b12032aa1fbaa6d8715061907d575bfff7f161e5
Richard Kennard <richard@kennardconsulting.com>
2013-06-05 20:34:58 +1000

    NodeJS
    Change-Id: I694bcede378d790f8ef60b82829cbe608769835e

--- modules/js/nodejs/src/main/js/test/render.js
+++ modules/js/nodejs/src/main/js/test/render.js
@@ -1,130 +1,134 @@
+var assert = require( 'assert' );
 var metawidget = require( 'metawidget' );
 
-describe( 'render', function() {
+// Use a simple DOM implementation
 
-	describe( 'the NodeJS plugin', function() {
+var simpleDocument = {
+	createElement: function( elementName ) {
 
-		it( 'should render Metawidget correctly', function( done ) {
+		return {
+			nodeType: 1,
+			tagName: elementName.toUpperCase(),
+			attributes: [],
+			childNodes: [],
+			setAttribute: function( name, value ) {
 
-			var myObj = {
-				"firstname": "Joe",
-				"surname": "Bloggs"
-			};
-
-			var simpleDocument = {
-				createElement: function( elementName ) {
-
-					return {
-						nodeType: 1,
-						tagName: elementName.toUpperCase(),
-						attributes: [],
-						childNodes: [],
-						setAttribute: function( name, value ) {
-
-							for ( var loop = 0, length = this.attributes.length; loop < length; loop++ ) {
-								if ( this.attributes[loop].nodeName === name ) {
-									this.attributes[loop].nodeValue = value;
-									return;
-								}
-							}
-
-							this.attributes.push( {
-								nodeName: name,
-								nodeValue: value
-							} );
-						},
-						hasAttribute: function( name ) {
-
-							for ( var loop = 0, length = this.attributes.length; loop < length; loop++ ) {
-								if ( this.attributes[loop].nodeName === name ) {
-									return true;
-								}
-							}
-
-							return false;
-						},
-						getAttribute: function( name ) {
-
-							for ( var loop = 0, length = this.attributes.length; loop < length; loop++ ) {
-								if ( this.attributes[loop].nodeName === name ) {
-									return this.attributes[loop].nodeValue;
-								}
-							}
-
-							return null;
-						},
-						appendChild: function( childNode ) {
-
-							this.childNodes.push( childNode );
-						},
-						cloneNode: function() {
-
-							var clone = simpleDocument.createElement( elementName );
-
-							for ( var loop = 0, length = this.attributes.length; loop < length; loop++ ) {
-								var attribute = this.attributes[loop];
-								clone.setAttribute( attribute.nodeName, attribute.nodeValue );
-							}
-							for ( var loop = 0, length = this.childNodes.length; loop < length; loop++ ) {
-								clone.appendChild( this.childNodes[loop].cloneNode() );
-							}
-							return clone;
-						},
-						removeChild: function( childNode ) {
-
-							for ( var loop = 0, length = this.childNodes.length; loop < length; loop++ ) {
-								if ( this.childNodes[loop] === childNode ) {
-									this.childNodes.splice( loop, 1 );
-									return childNode;
-								}
-							}
-
-							throw new Error( "childNode not found: " + childNode );
-						},
-						ownerDocument: this,
-						toString: function() {
-
-							var toString = "<" + elementName;
-
-							for ( var loop = 0, length = this.attributes.length; loop < length; loop++ ) {
-								var attribute = this.attributes[loop];
-								toString += ' ' + attribute.nodeName + '="' + attribute.nodeValue + '"';
-							}
-
-							toString += ">";
-
-							for ( var loop = 0, length = this.childNodes.length; loop < length; loop++ ) {
-								toString += this.childNodes[loop].toString();
-							}
-
-							if ( this.innerHTML !== undefined ) {
-								toString += this.innerHTML;
-							}
-
-							toString += "</" + elementName + ">";
-							return toString;
-						}
-					};
-				},
-				createTextNode: function( data ) {
-
-					return {
-						nodeType: 3,
-						toString: function() {
-
-							return data;
-						}
+				for ( var loop = 0, length = this.attributes.length; loop < length; loop++ ) {
+					if ( this.attributes[loop].nodeName === name ) {
+						this.attributes[loop].nodeValue = value;
+						return;
 					}
 				}
-			};
 
-			var element = simpleDocument.createElement( 'metawidget' );
-			var mw = new metawidget.Metawidget( element );
-			mw.toInspect = myObj;
-			mw.buildWidgets();
-			
-			expect( mw.toString() ).to.equal( '<metawidget></metawidget>' );
-			done();
-		} );
-	} );
-} );
+				this.attributes.push( {
+					nodeName: name,
+					nodeValue: value
+				} );
+			},
+			hasAttribute: function( name ) {
+
+				for ( var loop = 0, length = this.attributes.length; loop < length; loop++ ) {
+					if ( this.attributes[loop].nodeName === name ) {
+						return true;
+					}
+				}
+
+				return false;
+			},
+			getAttribute: function( name ) {
+
+				for ( var loop = 0, length = this.attributes.length; loop < length; loop++ ) {
+					if ( this.attributes[loop].nodeName === name ) {
+						return this.attributes[loop].nodeValue;
+					}
+				}
+
+				return null;
+			},
+			appendChild: function( childNode ) {
+
+				this.childNodes.push( childNode );
+			},
+			cloneNode: function() {
+
+				var clone = simpleDocument.createElement( elementName );
+
+				for ( var loop = 0, length = this.attributes.length; loop < length; loop++ ) {
+					var attribute = this.attributes[loop];
+					clone.setAttribute( attribute.nodeName, attribute.nodeValue );
+				}
+				for ( var loop = 0, length = this.childNodes.length; loop < length; loop++ ) {
+					clone.appendChild( this.childNodes[loop].cloneNode() );
+				}
+				return clone;
+			},
+			removeChild: function( childNode ) {
+
+				for ( var loop = 0, length = this.childNodes.length; loop < length; loop++ ) {
+					if ( this.childNodes[loop] === childNode ) {
+						this.childNodes.splice( loop, 1 );
+						return childNode;
+					}
+				}
+
+				throw new Error( "childNode not found: " + childNode );
+			},
+			ownerDocument: this,
+			toString: function() {
+
+				var toString = "<" + elementName;
+
+				for ( var loop = 0, length = this.attributes.length; loop < length; loop++ ) {
+					var attribute = this.attributes[loop];
+					toString += ' ' + attribute.nodeName + '="' + attribute.nodeValue + '"';
+				}
+
+				if ( this.value !== undefined ) {
+					toString += ' value="' + this.value + '"';
+				}
+
+				toString += ">";
+
+				for ( var loop = 0, length = this.childNodes.length; loop < length; loop++ ) {
+					toString += this.childNodes[loop].toString();
+				}
+
+				if ( this.innerHTML !== undefined ) {
+					toString += this.innerHTML;
+				}
+
+				toString += "</" + elementName + ">";
+				return toString;
+			}
+		};
+	},
+	createTextNode: function( data ) {
+
+		return {
+			nodeType: 3,
+			toString: function() {
+
+				return data;
+			}
+		}
+	}
+};
+
+var element = simpleDocument.createElement( 'metawidget' );
+var mw = new metawidget.Metawidget( element );
+mw.toInspect = {
+	name: "Joe Bloggs",
+	"DOB": "1/1/2001"
+};
+mw.buildWidgets();
+
+// Test what was rendered
+
+assert.ok( element.toString().indexOf( '<label for="name" id="table-name-label">Name:</label>' ) !== -1 );
+assert.ok( element.toString().indexOf( '<input type="text" id="name" name="name" value="Joe Bloggs"></input>' ) !== -1 );
+assert.ok( element.toString().indexOf( '<label for="DOB" id="table-DOB-label">DOB:</label>' ) !== -1 );
+assert.ok( element.toString().indexOf( '<input type="text" id="DOB" name="DOB" value="1/1/2001"></input>' ) !== -1 );
+assert
+		.equal(
+				element.toString(),
+				'<metawidget><table><tbody><tr id="table-name-row"><th id="table-name-label-cell"><label for="name" id="table-name-label">Name:</label></th><td id="table-name-cell"><input type="text" id="name" name="name" value="Joe Bloggs"></input></td><td></td></tr><tr id="table-DOB-row"><th id="table-DOB-label-cell"><label for="DOB" id="table-DOB-label">DOB:</label></th><td id="table-DOB-cell"><input type="text" id="DOB" name="DOB" value="1/1/2001"></input></td><td></td></tr></tbody></table></metawidget>' );
---
+++ modules/js/nodejs/src/main/resources/Readme.md
@@ -0,0 +1,39 @@
+# metawidget
+
+Metawidget ${project.version} is a smart User Interface widget that populates itself, at runtime, with UI components to match the properties of your business objects.
+
+## Introduction
+For an introduction to Metawidget, please see the tutorial at http://metawidget.org/doc/reference/en/html/ch01s02.html
+
+The Node.js version of Metawidget must be used in combination with a DOM implementation. This can either be jsdom, envjs, or even a simple implementation of your own (see test/render.js inside this module for an example). Metawidget must be wrapped around a DOM element. The Metawidget constructor takes this element, and thereafter always uses `element.ownerDocument` rather than referencing any global `document` object.
+
+## Installation
+`npm install metawidget`
+
+## Usage
+See test/render.js inside this module for a working example. But briefly:
+
+`var mw = new metawidget.Metawidget( element );
+mw.toInspect = {
+	firstname: "Joe",
+	surname: "Bloggs"
+};
+mw.buildWidgets();`
+
+## License
+
+(The LGPL License)
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
