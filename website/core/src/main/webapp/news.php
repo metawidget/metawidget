0117c84001d3a05bc324685526fe29c8947ec626
Richard Kennard <richard@kennardconsulting.com>
2013-10-23 21:19:57 +1100

    News
    Change-Id: I23417b09ab52a371b6aabffe43f1852360ad6ad0

--- website/core/src/main/webapp/news.php
+++ website/core/src/main/webapp/news.php
@@ -4,6 +4,29 @@
 	
 		<h2>News</h2>
 		
+		<h3>Metawidget v3.7 Released!</h3>
+		<h4></h4>
+		
+		<p>
+			Metawidget v3.6 is now available!
+		</p>
+		<p>
+			This release was focused on: 			  
+		</p>
+		
+		<ul>
+			<li>JavaScript support for collections of radio buttons</li>
+			<li>JacksonInspector</li>
+			<li>Android ReflectionBindingProcessor</li>
+			<li>Bug fixes, documentation and unit tests</li>
+		</ul>
+		
+		<p>
+			Your continued feedback is invaluable to us. Please <a href="download.php">download it</a> and <a href="support.php">let us know what you think</a>.
+		</p>
+
+		<hr/>
+
 		<h3>Metawidget v3.6 Released!</h3>
 		<h4>20th September 2013</h4>
 		
@@ -27,6 +50,7 @@
 		</p>
 
 		<hr/>
+		
 		<h3>Metawidget v3.5 Released!</h3>
 		<h4>27th July 2013</h4>
 		
