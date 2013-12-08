eeb2013075f557bc5d2299a85155326ff6be5397
Richard Kennard <richard@kennardconsulting.com>
2013-12-08 12:23:03 +1100

    Unit tests
    Change-Id: I99f95f3e6b3be91851460ab0677302fdd6f9c1fb

--- examples/js/jquery/mobile/addressbook/src/test/js/addressbook-jquerymobile-test.js
+++ examples/js/jquery/mobile/addressbook/src/test/js/addressbook-jquerymobile-test.js
@@ -74,69 +74,73 @@ describe( 'The JQuery Mobile AddressBook', function() {
 			$( document ).one( 'pageshow', '#contact-page', function( event ) {
 
 				page = $( event.target );
-				var mw = page.find( '#metawidget' );
+				
+				setTimeout( function() {
 
-				expect( mw.find( '#firstname' )[0].tagName ).toBe( 'OUTPUT' );
-				expect( mw.find( '#firstname' ).text() ).toBe( 'Homer' );
-				expect( mw.find( '#surname' )[0].tagName ).toBe( 'OUTPUT' );
-				expect( mw.find( '#surname' ).text() ).toBe( 'Simpson' );
-				expect( mw.find( '#communications tbody tr:eq(0) td:eq(0)' ).text() ).toBe( 'Telephone' );
-				expect( mw.find( '#communications tbody tr:eq(0) td:eq(1)' ).text() ).toBe( '(939) 555-0113' );
-				expect( mw.find( '#communications tbody tr:eq(0) td:eq(2)' )[0] ).toBeUndefined();
-
-				page.find( '#viewEdit' ).click();
-
-				mw.one( 'buildEnd', function() {
-
-					expect( mw.find( '#firstname' )[0].tagName ).toBe( 'INPUT' );
-					expect( mw.find( '#firstname' ).val() ).toBe( 'Homer' );
-					expect( mw.find( '#surname' )[0].tagName ).toBe( 'INPUT' );
-					expect( mw.find( '#surname' ).val() ).toBe( 'Simpson' );
+					var mw = page.find( '#metawidget' );
+	
+					expect( mw.find( '#firstname' )[0].tagName ).toBe( 'OUTPUT' );
+					expect( mw.find( '#firstname' ).text() ).toBe( 'Homer' );
+					expect( mw.find( '#surname' )[0].tagName ).toBe( 'OUTPUT' );
+					expect( mw.find( '#surname' ).text() ).toBe( 'Simpson' );
 					expect( mw.find( '#communications tbody tr:eq(0) td:eq(0)' ).text() ).toBe( 'Telephone' );
 					expect( mw.find( '#communications tbody tr:eq(0) td:eq(1)' ).text() ).toBe( '(939) 555-0113' );
-					expect( mw.find( '#communications tbody tr:eq(0) td:eq(2) button' ).val() ).toBe( 'Edit' );
-
-					mw.find( '#communications tbody tr:eq(0) td:eq(2) button' ).click();
-
-					$( document ).one( 'pageshow', '#communication-page', function( event ) {
-
-						var communicationPage = $( event.target );
-						expect( communicationPage.find( 'h1' ).text() ).toBe( 'Edit Communication' );
-
-						var communicationMw = communicationPage.find( '#metawidget' );
-
-						expect( communicationMw.find( '#type' )[0].tagName ).toBe( 'SELECT' );
-						expect( communicationMw.find( '#type' ).val() ).toBe( 'Telephone' );
-						expect( communicationMw.find( '#value' )[0].tagName ).toBe( 'INPUT' );
-						expect( communicationMw.find( '#value' ).val() ).toBe( '(939) 555-0113' );
-
-						communicationPage.find( '#editUpdate' ).click();
-
-						$( document ).one( 'pageshow', '#contact-page', function( event ) {
-
-							page = $( event.target );
-							page.find( '#viewEdit' ).click();
-
-							mw = page.find( '#metawidget' );
-
-							mw.one( 'buildEnd', function() {
-
-								mw.find( '#firstname' ).val( 'Homer Jay' );
-								page.find( '#editUpdate' ).click();
-
-								$( document ).one( 'pageshow', '#contacts-page', function( event ) {
-
-									page = $( event.target );
-									summary = page.find( '#summary' );
-									expect( summary.find( 'li:eq(0) a' ).text() ).toBe( 'Homer Jay Simpson' );
-									expect( summary.find( 'li:eq(1) a' ).text() ).toBe( 'Marjorie Simpson' );
-
-									_done = true;
+					expect( mw.find( '#communications tbody tr:eq(0) td:eq(2)' )[0] ).toBeUndefined();
+	
+					page.find( '#viewEdit' ).click();
+	
+					mw.one( 'buildEnd', function() {
+	
+						expect( mw.find( '#firstname' )[0].tagName ).toBe( 'INPUT' );
+						expect( mw.find( '#firstname' ).val() ).toBe( 'Homer' );
+						expect( mw.find( '#surname' )[0].tagName ).toBe( 'INPUT' );
+						expect( mw.find( '#surname' ).val() ).toBe( 'Simpson' );
+						expect( mw.find( '#communications tbody tr:eq(0) td:eq(0)' ).text() ).toBe( 'Telephone' );
+						expect( mw.find( '#communications tbody tr:eq(0) td:eq(1)' ).text() ).toBe( '(939) 555-0113' );
+						expect( mw.find( '#communications tbody tr:eq(0) td:eq(2) button' ).val() ).toBe( 'Edit' );
+	
+						mw.find( '#communications tbody tr:eq(0) td:eq(2) button' ).click();
+	
+						$( document ).one( 'pageshow', '#communication-page', function( event ) {
+	
+							var communicationPage = $( event.target );
+							expect( communicationPage.find( 'h1' ).text() ).toBe( 'Edit Communication' );
+	
+							var communicationMw = communicationPage.find( '#metawidget' );
+	
+							expect( communicationMw.find( '#type' )[0].tagName ).toBe( 'SELECT' );
+							expect( communicationMw.find( '#type' ).val() ).toBe( 'Telephone' );
+							expect( communicationMw.find( '#value' )[0].tagName ).toBe( 'INPUT' );
+							expect( communicationMw.find( '#value' ).val() ).toBe( '(939) 555-0113' );
+	
+							communicationPage.find( '#editUpdate' ).click();
+	
+							$( document ).one( 'pageshow', '#contact-page', function( event ) {
+	
+								page = $( event.target );
+								page.find( '#viewEdit' ).click();
+	
+								mw = page.find( '#metawidget' );
+	
+								mw.one( 'buildEnd', function() {
+	
+									mw.find( '#firstname' ).val( 'Homer Jay' );
+									page.find( '#editUpdate' ).click();
+	
+									$( document ).one( 'pageshow', '#contacts-page', function( event ) {
+	
+										page = $( event.target );
+										summary = page.find( '#summary' );
+										expect( summary.find( 'li:eq(0) a' ).text() ).toBe( 'Homer Jay Simpson' );
+										expect( summary.find( 'li:eq(1) a' ).text() ).toBe( 'Marjorie Simpson' );
+	
+										_done = true;
+									} );
 								} );
 							} );
 						} );
 					} );
-				} );
+				}, 1000 );
 			} );
 		} );
 
