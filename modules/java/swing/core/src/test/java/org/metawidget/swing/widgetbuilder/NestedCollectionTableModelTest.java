7b99181309033cee28ecb1d2251d9df8fe6d0826
Richard Kennard <richard@kennardconsulting.com>
2014-05-27 07:46:10 +1000

    Prep for release
    Change-Id: Id7ced1b5268100ef241096ea9eb17a4b6afdf1f7

--- modules/java/swing/core/src/test/java/org/metawidget/swing/widgetbuilder/NestedCollectionTableModelTest.java
+++ modules/java/swing/core/src/test/java/org/metawidget/swing/widgetbuilder/NestedCollectionTableModelTest.java
@@ -1,3 +1,14 @@
+// Metawidget
+//
+// This file is dual licensed under both the LGPL
+// (http://www.gnu.org/licenses/lgpl-2.1.html) and the EPL
+// (http://www.eclipse.org/org/documents/epl-v10.php). As a
+// recipient of Metawidget, you may choose to receive it under either
+// the LGPL or the EPL.
+//
+// Commercial licenses are also available. See http://metawidget.org
+// for details.
+
 package org.metawidget.swing.widgetbuilder;
 
 import java.util.ArrayList;
